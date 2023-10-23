4466b5bba721b914c4c0bce735ff182d75ab8e26
high-haseeb <haseebkhalidoriginal@gmail.com>
2023-10-23 17:54:00 +0500

    init

---
+++ components/rotatingText/rotating.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./rotating.css";
+
+const RotatingText = () => {
+  return (
+    <svg
+      width="300"
+      height="300"
+      viewBox="0 0 79.374998 79.375"
+      version="1.1"
+      id="svg1"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsSvg="http://www.w3.org/2000/svg"
+    >
+      <defs id="defs1" />
+      <g  id="layer1">
+        <circle
+          styles="fill:none;stroke:none;stroke-width:0.381736"
+          id="path1"
+          cx="39.6875"
+          cy="39.6875"
+          r="28.439157"
+        />
+        <text
+          xmlSpace="preserve"
+          styles="font-size:2.175px;fill:none;stroke:#000000;stroke-width:0.264999"
+          id="text1"
+        >
+          <textPath
+            xlinkHref="#path1"
+            id="textPath1"
+            styles="font-style:normal;font-variant:normal;font-weight:600;font-stretch:normal;font-size:2.3472px;font-family:Sans;-inkscape-font-specification:'Sans, Semi-Bold';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;stroke:none;stroke-width:0.265"
+          >
+            we do it better.we do it better.
+          </textPath>
+        </text>
+      </g>
+    </svg>
+  );
+};
+
+export default RotatingText;
